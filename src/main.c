9c45e78a23979b8fb0834247433472d7e8ceb047
Dominick <dominickhera@me.com>
2016-04-28 19:49:27 -0400

    added more files

--- src/main.c
+++ src/main.c
@@ -1,8 +1,17 @@
-#include <stdio.h>
+#include <function.h>
 
-int main()
+int main(int argc, char* argv[])
 {
-	printf("hello\n");
+	if(argc != 3)
+	{
+		printf("not enough arguments\n");
+		exit(1);
+	}
+	else
+	{
+		printf("%s\n", argv[1]);
+		printf("%s\n", argv[2]);
+	}
 
 	return 0;
 }
